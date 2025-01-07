0797c14c8b17648e0e049c009f99a1a95bbd3f2e
ustinian-wang <wangjser@gmail.com>
2025-01-08 02:40:44 +0800

    feat: update color kits

--- src/utils/color.js
+++ src/utils/color.js
@@ -18,7 +18,7 @@ export function colorToHex(rgb) {
         return rgb;
     }
     let arr = rgb.replace("(").replace(")").split(",")
-    return rgbStr2hex(arr[0], arr[1], arr[2]);
+    return rgbArgs2hex(arr[0], arr[1], arr[2]);
 }
 
 /**
@@ -28,6 +28,44 @@ export function colorToHex(rgb) {
  * @param {number|string} blue
  * @returns {string}
  */
-export function rgbStr2hex(red, green, blue) {
+export function rgbArgs2hex(red, green, blue) {
     return '#' + [red, green, blue].map(value=>parseInt(value || '0').toString(16))
+}
+
+/**
+ * @description hex to rgb
+ * @param {string} hex
+ * @returns {Array<number>}
+ */
+export function hex2rgb(hex) {
+    hex = hex + "";
+    hex = hex.startsWith("#") ? hex.slice(1) : hex;
+    let r = parseInt(hex.slice(0, 2), 16);
+    let g = parseInt(hex.slice(2, 4), 16);
+    let b = parseInt(hex.slice(4), 16);
+  
+    return [r, g, b];
+}
+
+/**
+ * @description rgb to hex
+ * @param {string} color
+ * @returns {string}
+ */
+export function rgbToHex(color) {
+    var values = color
+      .replace(/rgb?\(/, "")
+      .replace(/\)/, "")
+      .replace(/[\s+]/g, "")
+      .split(",");
+    var a = parseFloat((values[3] || 1) + ""),
+      r = Math.floor(a * parseInt(values[0]) + (1 - a) * 255),
+      g = Math.floor(a * parseInt(values[1]) + (1 - a) * 255),
+      b = Math.floor(a * parseInt(values[2]) + (1 - a) * 255);
+    return (
+      "#" +
+      ("0" + r.toString(16)).slice(-2) +
+      ("0" + g.toString(16)).slice(-2) +
+      ("0" + b.toString(16)).slice(-2)
+    );
 }
